86c5edfd0b8e83e68e668dad3de28c48fc020fc4
petchkub <119925016+petchkub@users.noreply.github.com>
2024-02-16 11:22:41 +0700

    put books id

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
   // server.js
   const express = require('express')
   const bodyParser = require('body-parser')
-  
+  const app = express()
+
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: true }))
-
-  const app = express()
 
   app.get('/', (req, res) => {
     res.send('Hello World')
@@ -22,6 +21,11 @@
 
   app.post('/books', (req, res) => {
     books.push(req.body)
+  })
+
+  app.put('/books/:id', (req, res) => {
+    const updateIndex = books.findIndex(book => book.id === req.params.id)
+    res.json(Object.assign(books[updateIndex], req.body))
   })
   
   app.listen(3000, () => {
