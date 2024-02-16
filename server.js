d4a6c4a2ab90a2650314335068ffa58ac473972a
petchkub <119925016+petchkub@users.noreply.github.com>
2024-02-16 10:41:42 +0700

    server&db

---
+++ server.js
@@ -0,0 +1,16 @@
+  // server.js
+  const express = require('express')
+  const app = express()
+
+  app.get('/', (req, res) => {
+    res.send('Hello World')
+  })
+
+  const books = require('./db')
+  app.get('/books', (req, res) => {
+    res.json(books)
+  })
+
+  app.listen(3000, () => {
+    console.log('http://localhost:3000')
+  })
