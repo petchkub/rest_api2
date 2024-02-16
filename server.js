1970efd42656f56d11af851178cfd7723f8af3fa
petchkub <119925016+petchkub@users.noreply.github.com>
2024-02-16 10:51:09 +0700

    book&bookid

--- server.js
+++ server.js
@@ -11,6 +11,10 @@
     res.json(books)
   })
 
+  app.get('/books/:id', (req, res) => {
+    res.json(books.find(book => book.id === req.params.id))
+  })
+
   app.listen(3000, () => {
     console.log('http://localhost:3000')
   })
