6a076aff80f2a606e547a5189da520d40b3a9430
petchkub <119925016+petchkub@users.noreply.github.com>
2024-02-23 11:04:26 +0700

    add status in put and delete

--- server.js
+++ server.js
@@ -7,31 +7,36 @@
   app.use(bodyParser.urlencoded({ extended: true }))
 
   app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.status(200).send('Hello World')
   })
 
+  //select
   const books = require('./db')
   app.get('/books', (req, res) => {
     res.json(books)
   })
 
   app.get('/books/:id', (req, res) => {
-    res.json(books.find(book => book.id === req.params.id))
+    res.status(200).json(books.find(book => book.id === req.params.id))
   })
 
+  //insert
   app.post('/books', (req, res) => {
     books.push(req.body)
     res.status(201).json(req.body)
   })
 
+  //update
   app.put('/books/:id', (req, res) => {
     const updateIndex = books.findIndex(book => book.id === req.params.id)
-    res.json(Object.assign(books[updateIndex], req.body))
+    res.status(200).json(Object.assign(books[updateIndex], req.body))
   })
   
+  //delete
   app.delete('/books/:id', (req, res) => {
     const deletedIndex = books.findIndex(book => book.id === req.params.id)
     delete books[deleteIndex];
+    res.status(200).json(req.body)
  })
 
   app.listen(3000, () => {
